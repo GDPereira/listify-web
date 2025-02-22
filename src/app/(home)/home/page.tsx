e09b59087d51e00c27aa17762b0fcb0ce6d7da2f
Guilherme Deluca <gdelucapereira@gmail.com>
2025-02-22 12:43:40 -0300

    feat: created new home screen

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -1,9 +1,13 @@
-const HomePage = () => {
-  return (
-    <div>
-      <span>Home Page</span>
-    </div>
-  );
-};
+"use client";
 
-export default HomePage;
+import { Header } from "@/components/header";
+import "@ant-design/v5-patch-for-react-19";
+import { Layout } from "antd";
+
+export default function HomePage() {
+  return (
+    <Layout className="h-dvh">
+      <Header />
+    </Layout>
+  );
+}
